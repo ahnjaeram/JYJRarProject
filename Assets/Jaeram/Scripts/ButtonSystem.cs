b2f5e5c58f2a87990dc1cd55f70fc2da7002ae3d
ahnjaeram <ahnjaeram@gamil.com>
2020-12-01 16:22:31 +0900

    색깔 바꾸기 적용
    색깔 바꾸기
    그릴때 UI 안보이게

---
+++ Assets/Jaeram/Scripts/ButtonSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSystem : MonoBehaviour
+{
+    Draw draw;
+    void Start()
+    {
+        draw = GameObject.Find("Draw").GetComponent<Draw>();
+    }
+
+    // Update is called once per frame
+    public void SetColorWhite()
+    {
+        draw.lineColor = Color.white;
+    }
+    public void SetColorRed()
+    {
+        draw.lineColor = Color.red;
+    }
+    public void SetColorOrange()
+    {
+        draw.lineColor = new Color(1,0.5f,0);
+    }
+    public void SetColorYellow()
+    {
+        draw.lineColor = Color.yellow;
+    }
+    public void SetColorGreen()
+    {
+        draw.lineColor = Color.green;
+    }
+    public void SetColorBlue()
+    {
+        draw.lineColor = new Color(0, 0.4f, 1);
+    }
+    public void SetColorDarkBlue()
+    {
+        draw.lineColor = new Color(0, 0, 1);
+    }
+    public void SetColorPurple()
+    {
+        draw.lineColor = new Color(0.5f, 0, 1);
+    }
+}
--- Assets/Jaeram/Scripts/Draw.cs
+++ Assets/Jaeram/Scripts/Draw.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Draw : MonoBehaviour
 {
-
+    EventTrigger eTrigger;
     LineRenderer lr;
     Vector3[] lineVertices = new Vector3[10000000];
    // Vector3[] lineVerticestoAdd = new Vector3[2];
@@ -13,19 +15,32 @@ public class Draw : MonoBehaviour
     public int verticeIdx = 0;
     int lineNumb = 0;
     GameObject graffiti;
+    public Color lineColor=Color.white;
+    GameObject colorSelection;
+    public Image[] buttonColors=new Image[8];
     // Start is called before the first frame update
     void Start()
     {
         //lr =GameObject.Find("Line").GetComponent<LineRenderer>();
         //lr.enabled = false;
         graffiti = GameObject.Find("Graffiti");
+        colorSelection= GameObject.Find("ColorSelection");
+
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         //클릭 할때마다 새로운 라인 렌더러를 만든다
-        if (Input.touchCount > 0)
+        //GameObject go = EventSystem.current.currentSelectedGameObject;
+        //if (go != null)
+        //{
+        //    print(go.name);
+        //}
+
+        if (Input.touchCount > 0&& !EventSystem.current.currentSelectedGameObject)
         {
             if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
@@ -36,11 +51,19 @@ public class Draw : MonoBehaviour
                 Material whiteDiffuseMat = new Material(Shader.Find("Sprites/Default"));
                 lr = lineDrawer.GetComponent<LineRenderer>();
                 lr.material = whiteDiffuseMat;
-                lr.material.color = Color.white;
+                //lr.material.color = Color.white;
+                lr.material.color = lineColor;
                 lr.startWidth = 0.1f;
                 lr.endWidth = 0.1f;
                 lr.numCornerVertices = 5;
                 lr.numCapVertices = 5;
+
+                for(int i = 0; i < buttonColors.Length; i++)
+                {
+                    StartCoroutine(Hide(buttonColors[i]));
+                }
+
+                
                 
             }
         //움직임에 따라 라인이 그려진다
@@ -62,40 +85,43 @@ public class Draw : MonoBehaviour
                     verticeIdx = 0;
                     lineNumb++;
                 }
+
+                for (int i = 0; i < buttonColors.Length; i++)
+                {
+                    StartCoroutine(Show(buttonColors[i]));
+                }
             }
 
 
-
-
-            //if (Input.GetTouch(0).phase == TouchPhase.Began)
-            //{
-            //    verticeIdx = 0;
-            //    lineVertices[verticeIdx] = verticePos;
-            //}
-            ////플레이어가 화면을 터치하면
-            //if (Input.GetTouch(0).phase == TouchPhase.Moved)
-            //{
-            //    lineVerticestoAdd[0] = lineVertices[verticeIdx];
-            //    verticeIdx += 1;
-            //    lineVertices[verticeIdx] = verticePos;
-            //    lineVerticestoAdd[1] = lineVertices[verticeIdx];
-            //    lr.SetPositions(lineVerticestoAdd);
-
-                //}
-                //else if (Input.GetTouch(0).phase == TouchPhase.Ended)
-                //{
-                //    print("선긋기 끝");
-                //}
-                //그 부분부터 손을 대고 있는 동안 선을 그리기 시작한다
-                //손을 때면 끝난다.
         }
-        //else
-        //{
-        //    if (verticeIdx != 0)
-        //    {
-        //        verticeIdx = 0;
-        //    }
-        //}
+     
+    }
+    public float hideShowTime=0.5f;
+    
+    
+    IEnumerator Hide(Image image)
+    {
+        float currentTime = 0;
+        while (currentTime / hideShowTime < 1)
+        {
+            currentTime += Time.deltaTime;
+            float a = Mathf.Lerp(1, 0, currentTime / hideShowTime);
+            image.color=new Color(image.color.r,image.color.g,image.color.b,a);
+            yield return null;
+        }
+        
+    } 
+    IEnumerator Show(Image image)
+    {
+        float currentTime = 0;
+        while (currentTime / hideShowTime < 1)
+        {
+            currentTime += Time.deltaTime;
+            float a=Mathf.Lerp(0, 1, currentTime / hideShowTime);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, a);
+            yield return null;
+        }
+        
     }
 }
 
